7a994682ec82d9083d80d73b7e6bd267d8f0a888
Ayxan Huseynov <ayxanhuseynov200519@gmail.com>
2025-12-15 16:56:45 +0330

    Add files via upload

---
+++ JS Methods/assets/js/main.js
@@ -0,0 +1,52 @@
+// TASK 1
+// 1. const arr = [1,2,4,12,56,547] bu arrayde butun ededleri 2-ye vurub yeni array yaradin
+// const arr = [1, 2, 4, 12, 56, 547];
+
+// const newArr = arr.map(num => num * 2);
+
+// console.log(newArr);
+
+// TASK 2
+// 2. const arr = [5,8,11,20,7] bu arrayda yalniz cut ededleri sechin
+// const arrr = [5, 8, 11, 20, 7];
+
+// const evenNumbers = arrr.filter(num => num % 2 === 0);
+
+// console.log(evenNumbers);
+
+// TASK 3
+// 3. const arr = ["alma", "armud", "chiyelek", "mandalina"] arrayde neche element oldugunu tap
+// const arrrr = ["alma", "armud", "chiyelek", "mandalina"];
+
+// const count = arr.length;
+
+// console.log(count);
+
+// TASK 4
+//4. const arr = [3,6,9] arrayde butun ededlerin cemini tapn
+// const ar = [3, 6, 9];
+
+// const sum = arr.reduce((total, num) => total + num, 0);
+
+// console.log(sum);
+
+// TASK 5
+//5. const arr = ["c#", "sql", "html", "css", "scss", "js", "react"] arrayde js sozunun olub olmadigini yoxla
+const aar = ["c#", "sql", "html", "css", "scss", "js", "react"];
+const ayxan = aar.includes("js");
+console.log(ayxan);
+
+// TASK 6 
+
+
+
+
+
+
+
+
+
+
+
+
+
